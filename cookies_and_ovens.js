d261cf42690c5254006151d02cac12710076cd44
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-10 16:33:33 +0700

    release 0

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,49 @@
 // Answer These Questions:
 //
 // - What are essential classes?
+// Cake, PeanutCake, ChocoCake, CheeseCake Oven?
 // - What attributes will each class have?
+// Cake has heat_time
 // - What interface will each class provide?
+// 
 // - How will the classes interact with each other?
+// 
 // - Which classes will inherit from others, if any?
+// PeanutCake, ChocoCake, CheeseCake will inherit Cake
 //
 //
 // Your code here
+class Cake {
+    constructor(heatTime) {
+        this.heatTime = heatTime;
+        this.status = 'mentah';
+    }
+}
+
+class PeanutCake {
+    constructor() {
+        super(20);
+    }
+}
+
+class ChocoCake {
+    constructor() {
+        super(30);
+    }
+}
+
+class CheeseCake {
+    constructor() {
+        super(35);
+    }
+}
+
+class Oven {
+    constructor() {
+        this.cakes = []
+    }
+
+    heatCakes() {
+        
+    }
+}
