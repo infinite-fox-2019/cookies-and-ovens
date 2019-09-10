85c30e1113245f52237b3577e9130cc4f770d8a4
Panji SN <panji1595@gmail.com>
2019-09-10 18:12:06 +0700

    update

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,49 +1,58 @@
 class Cookies {
     constructor(name){
         this._name = name;
-        this._status = "mentah";
+        this._bakeTime = 0;
+        if(name == "Kue cokelat"){
+            this._waktuMatang = 20;
+        } else if (name == "Kue keju"){
+            this._waktuMatang = 30;
+        } else if(name == "Kue kacang"){
+            this._waktuMatang = 35;
+        } else {
+            this._waktuMatang = 25;
+        }
+    }
+
+    get status(){
+        if(this._bakeTime === this._waktuMatang){
+            return 'matang';
+        } else if (this._bakeTime+5 == this._waktuMatang){
+            return "hampir matang";
+        } else if (this._bakeTime < this._waktuMatang){
+            return "mentah";
+        } else {
+            return "gosong";
+        }
     }
 }
 class Oven {
-    constructor(name){
-        this._name = name;
-        this._waktuAwal = 0;
-        if(name._name == "Kue cokelat"){
-            this._waktuMatang = 20;
-        } else if(name._name == "Kue kacang"){
-            this._waktuMatang = 30;
-        } else if(name._name == "Kue keju"){
-            this._waktuMatang = 35;
-        } else {
-            this._waktuMatang = 40;
-        }
+    constructor(){
+        this.cookies = [];
     }
+
+    addCookie(cookie){
+        this.cookies.push(cookie)
+    }
+
     bake(waktu){
-        while(this._waktuAwal < waktu){
-            this._waktuAwal += 5;
-            if(this._waktuAwal+5 == this._waktuMatang){
-                this._name._status = "hampir matang"
-            } else if (this._waktuAwal == this._waktuMatang){
-                this._name._status = "matang";
-            } else if (this._waktuAwal < this._waktuMatang){
-                this._name._status = "mentah";
-            } else {
-                this._name._status = "gosong";
+        for(let i = 0; i < this.cookies.length; i++){
+            while (this.cookies[i]._bakeTime < waktu){
+                this.cookies[i]._bakeTime += 5;
+                console.log(`${this.cookies[i]._name},menit ke ${this.cookies[i]._bakeTime} : ${this.cookies[i].status}`);
             }
-            console.log(`${this._name._name},menit ke ${this._waktuAwal} : ${this._name._status}`);
+            // console.log(this.cookies._)
+            console.log(`Sudah selesai memanggang, ${this.cookies[i]._name} yang kamu masak ${this.cookies[i].status}`)
         }
-        console.log(`Sudah selesai memanggang, ${this._name._name} yang kamu masak ${this._name._status}`)
     }
 } 
 const kueKacang = new Cookies("Kue kacang");
 const kueCokelat = new Cookies("Kue cokelat");
 const kueKeju = new Cookies("Kue keju");
-const panggangKueCokelat = new Oven(kueCokelat);
-const panggangKueKacang = new Oven(kueKacang);
-const panggangKueKeju = new Oven(kueKeju);
-const statusKueCokelat = panggangKueCokelat.bake(30);
-const statusKueKeju = panggangKueKeju.bake(40);
-const statusKueKacang = panggangKueKacang.bake(30);
-console.log(statusKueCokelat);
-console.log(statusKueKacang);
-console.log(statusKueKeju);
+
+const oven = new Oven();
+
+oven.addCookie(kueKeju);
+oven.addCookie(kueCokelat);
+oven.addCookie(kueKacang);
+
+oven.bake(40);
