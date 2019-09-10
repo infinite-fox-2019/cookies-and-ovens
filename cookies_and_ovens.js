55a40739d6c6e2e8fd890a4b21bf1d3815f430f9
dadepe <rizky_dwifutra@yahoo.com>
2019-09-10 22:54:33 +0700

    adding cookiesNovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,66 @@
 //
 //
 // Your code here
+
+class Oven{
+
+    bake(kue, waktu){
+        for(let i = 0; i<=waktu; i+= 5){
+            for(let j = 0; j<kue.length; j++){
+                kue[j].time = i
+                console.log(`${kue[j].name}, menit ke ${i} : ${kue[j].status()}`)
+            }
+        }
+    }
+}
+
+class Kue{
+    constructor(nama, matang){
+        this.name=nama
+        this.time=0
+        this.cooked=matang
+    }
+
+    
+    status(){
+        let hasil = ""
+        if(this.time>=this.cooked+5){
+            hasil = "hangus"
+        }else if(this.time>=this.cooked){
+            hasil = "matang"
+        }else if(this.time>=this.cooked-5){
+            hasil = "belum matang"
+        }else{
+            hasil = "mentah"
+        }
+        return hasil
+        
+    }
+}
+
+class Coklat extends Kue{
+    constructor(){
+        super("Kue Coklat",20)
+
+    }
+}
+
+class Kacang extends Kue{
+    constructor(){
+        super("Kue Kacang",30)
+    }
+}
+
+class Keju extends Kue{
+    constructor(){
+        super("Kue Keju",35)
+    }
+}
+
+
+const oven = new Oven()
+const coklat = new Coklat()
+const kacang = new Kacang()
+const keju = new Keju()
+
+oven.bake([coklat,kacang,keju], 40)
