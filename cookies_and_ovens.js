55d79c73f4ad2ceb9d1596ed8df271c203fd7fd9
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-10 13:28:30 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,67 @@
 //
 //
 // Your code here
+
+class Cake {
+    constructor ( cakeName , time ) {
+        this.cakeName = cakeName ;
+        this.time = time;
+        this.start = 0;
+        this.status = '';
+    }
+
+    baking () {
+        this.start += 5
+        if ( this.start > this.time ) this.status = 'Hangus!';
+        else if ( this.start === this.time ) this.status = 'Matang!';
+        else if ( this.start == this.time -5 ) this.status = 'Hampir Matang!';
+        else this.status = 'Mentah!';
+    }
+}
+
+class Cokelat extends Cake {
+    constructor ( cakeName ) {
+        super ( cakeName , 20 );
+    }
+}
+
+class Kacang extends Cake {
+    constructor ( cakeName ) {
+        super ( cakeName , 30 );
+    }
+}
+
+class Keju extends Cake {
+    constructor ( cakeName ) {
+        super ( cakeName , 35 );
+    }
+}
+
+const kue1 = new Cokelat ( 'Kue Cokelat' );
+const kue2 = new Kacang ( 'Kue Kacang' );
+const kue3 = new Keju ( 'Kue Keju' );
+
+console.log(' ============================================================================');
+console.log(`Make ${kue1.cakeName}`);
+
+for (  let i=0; i<kue1.time+5; i+=5 ) {
+    kue1.baking()
+    console.log(`${kue1.cakeName}, menit ke ${kue1.start} : ${kue1.status}`)
+}
+
+console.log(' ============================================================================');
+console.log(`Make ${kue2.cakeName}`);
+
+for (  let i=0; i<kue2.time+5; i+=5 ) {
+    kue2.baking()
+    console.log(`${kue2.cakeName}, menit ke ${kue2.start} : ${kue2.status}`)
+}
+
+console.log(' ============================================================================');
+console.log(`Make ${kue3.cakeName}`);
+
+for (  let i=0; i<kue3.time+5; i+=5 ) {
+    kue3.baking()
+    console.log(`${kue3.cakeName}, menit ke ${kue3.start} : ${kue3.status}`)
+}
+
