d39ea19ad2029885ecdb4cc103b39c8899802230
ashirogisen <m.rizki.aiman@gmail.com>
2019-09-10 16:38:58 +0700

    updated with inheritance

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,15 +1,13 @@
 // Answer These Questions:
 //
 // - What are essential classes?
-// Answer: Kue and Oven, I can make KueCoklat, KueKeju, and KuePeanut if I want, but I think it's not necessary
+// Answer: Kue and Oven
 // - What attributes will each class have?
-// Answer: Class-kue will have raw-time, almost done-time, done-time, overcooked-time, and class-oven will have a method that require cake's (object from class-kue) information
-// - What interface will each class provide?
-// Answer: Class-kue will display the cake's name and the minutes it will cooked on oven, and class-oven will display the process of cook (method)
+// Answer: KueCoklat-class, KueKacang-class, and KueKeju-class will have raw-time, almost done-time, done-time, overcooked-time (inherited from Kue-Class), and Oven-class will have a method that require cake's information
 // - How will the classes interact with each other?
-// Answer: Put cake's information (attribute) on class-oven, then oven will start to cook (oven's method)
+// Answer: Put cake's (KueCoklat and friends) information (attribute) on Oven-Class, then oven will start to cook (oven's method)
 // - Which classes will inherit from others, if any?
-// Answer: Nothing
+// Answer: KueCoklat-class, KueKacang-class, and KueKeju-class will inherit from Kue-class
 
 
 // Your code here
@@ -26,6 +24,24 @@ class Kue {
 
     display() { //For Release 0
         return `${this.name}, waktu memanggang di dalam oven ${this.waktu} menit`
+    }
+}
+
+class KueCoklat extends Kue {
+    constructor(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus) {
+        super(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus)
+    }
+}
+
+class KueKacang extends Kue {
+    constructor(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus) {
+        super(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus)
+    }
+}
+
+class KueKeju extends Kue {
+    constructor(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus) {
+        super(name, waktu, waktu_hampir_matang, waktu_matang, waktu_hangus)
     }
 }
 
@@ -51,20 +67,22 @@ class Oven {
 }
 
 //Release 0
-// const KueCoklat = new Kue('Kue Coklat', 20)
-// const KueKacang = new Kue('Kue Kacang', 20)
-// const KueKeju = new Kue('Kue Keju', 20)
-// console.log(KueCoklat.display())
-// console.log(KueKacang.display())
-// console.log(KueKeju.display())
+// const KCoklat = new KueCoklat('Kue Coklat', 20)
+// const KKacang = new KueKacang('Kue Kacang', 20)
+// const KKeju = new KueKeju('Kue Keju', 20)
+// console.log(KCoklat.display())
+// console.log()
+// console.log(KKacang.display())
+// console.log()
+// console.log(KKeju.display())
 
-//Release 1
-const KueCoklat = new Kue('Kue Coklat', 0, 15, 20, 25)
-const KueKacang = new Kue('Kue Kacang', 0, 15, 20, 25)
-const KueKeju = new Kue('Kue Keju', 0, 15, 20, 25)
+//Release 1 dan 2
+const KCoklat = new KueCoklat('Kue Coklat', 0, 15, 20, 25)
+const KKacang = new KueKacang('Kue Kacang', 0, 15, 20, 25)
+const KKeju = new KueKeju('Kue Keju', 0, 15, 20, 25)
 
-Oven.cook(KueCoklat)
+Oven.cook(KCoklat)
 console.log()
-Oven.cook(KueKacang)
+Oven.cook(KKacang)
 console.log()
-Oven.cook(KueKeju)
+Oven.cook(KKeju)
