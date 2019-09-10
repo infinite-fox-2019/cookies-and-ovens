c3c191f07a447dec70c27b84386d90b583937356
anggabanny <anggabanny@gmail.com>
2019-09-10 10:35:21 +0700

    lagiSemangat

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,62 @@
 //
 //
 // Your code here
+class Cookie{
+    constructor(){
+        this.name = 'Cookie'
+        this.status = 'mentah'
+        this.cookingTime = 0
+    }
+
+    baking(time){
+        for(let i = 5; i <= time; i += 5){
+            if (i < this.cookingTime -10){
+                this.status = 'mentah'
+            }else if(i === this.cookingTime -5){
+                this.status = 'hampir matang'
+            }else if(i === this.cookingTime){
+                this.status = 'matang'
+            }else if(i > this.cookingTime){
+                this.status = 'hangus'
+            }
+            console.log(`${this.name}, menit ke ${i} : ${this.status}`)
+        }
+    }
+
+}
+
+class Chocolate extends Cookie{
+    constructor(){
+        // super()
+        this.name = 'Kue Coklat'
+        this.cookingTime = 20
+    }
+}
+
+class Peanut extends Cookie{
+    constructor(){
+        // super()
+        this.name = 'Kue Kacang'
+        this.cookingTime = 30
+    }
+}
+
+class Cheese extends Cookie{
+    constructor(){
+        // super()
+        this.name = 'Kue Keju'
+        this.cookingTime = 35
+    }
+}
+
+console.log('-- Masak kue Coklat Enak --');
+let chocolate = new Chocolate()
+chocolate.baking(25)
+console.log('\n');
+console.log('-- Masak kue Kacang Enak --');
+let peanut = new Peanut()
+peanut.baking(35)
+console.log('\n');
+console.log('-- Masak kue Keju Enak --');
+let cheese = new Cheese()
+cheese.baking(40)
